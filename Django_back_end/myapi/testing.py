e1bb1682d23e3f0aaaea684fcbc95f7989306198
Seandowling123 <dowling.sean103@gmail.com>
2024-05-06 18:21:02 +0100

    get_order_by_client_order_id

--- Django_back_end/myapi/testing.py
+++ Django_back_end/myapi/testing.py
@@ -1,26 +1,12 @@
 import alpaca_trade_api as tradeapi
 from API_keys import API_KEY, SECRET_KEY
 
-print(API_KEY)
-
 # Initialize Alpaca API
 api = tradeapi.REST(API_KEY, SECRET_KEY, base_url='https://paper-api.alpaca.markets')
 
-order_list = api.list_orders(status='closed')
-
 order_id='test_order'
-specific_order = api.get_order(order_id)
+specific_order = api.get_order_by_client_order_id(order_id)
 print(specific_order)
-
-"""# Place an order
-order = api.submit_order(
-    symbol='AAPL',
-    qty=10,
-    side='buy',
-    type='market',
-    time_in_force='gtc',
-    client_order_id='test_order'
-)"""
 
 """# Check order status
 order_status = order.status
