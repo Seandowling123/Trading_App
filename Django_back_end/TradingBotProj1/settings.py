5ba89634f75bb708fe92a479f92784b8bfdcf099
Seandowling123 <dowling.sean103@gmail.com>
2024-05-07 20:47:38 +0100

    fixed

--- Django_back_end/TradingBotProj1/settings.py
+++ Django_back_end/TradingBotProj1/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "build/static")
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "build/static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
