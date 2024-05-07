5b75ee651c2a178def19bfc1a9093e74c81fcc46
Seandowling123 <dowling.sean103@gmail.com>
2024-05-07 12:11:22 +0100

    move finance tools to folder

--- Django_back_end/myapi/views.py
+++ Django_back_end/myapi/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import HttpResponse
-from . import get_financial_data
+from .finance_tools import get_financial_data
 
 @api_view(['GET'])
 def hello_world(request):
