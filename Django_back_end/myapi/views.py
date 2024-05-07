41e2af1ab41c80cbaa7e7ff2b04d4d230399afee
Seandowling123 <dowling.sean103@gmail.com>
2024-05-07 12:37:44 +0100

    move react to static folder

--- Django_back_end/myapi/views.py
+++ Django_back_end/myapi/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import HttpResponse
 from .finance_tools import get_financial_data
+import os
 
 @api_view(['GET'])
 def hello_world(request):
@@ -16,3 +17,13 @@ def asdf(request):
 def historical_data(request, ticker):
     response = get_financial_data.get_close_with_bands(ticker)
     return Response({'financial_data': response})
+
+@api_view(['GET'])
+def index(request):
+    # Get the path to the main HTML file of your React app
+    react_app_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'path_to_react_app', 'index.html')
+    # Read the HTML file
+    with open(react_app_path, 'r') as file:
+        html_content = file.read()
+    # Serve the HTML content
+    return HttpResponse(html_content)
