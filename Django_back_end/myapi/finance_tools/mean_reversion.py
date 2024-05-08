c7461f7f9e87f8da607116f05622619d057d000c
Seandowling123 <dowling.sean103@gmail.com>
2024-05-08 19:18:05 +0100

    buy condition

--- Django_back_end/myapi/finance_tools/mean_reversion.py
+++ Django_back_end/myapi/finance_tools/mean_reversion.py
@@ -2,6 +2,7 @@ import threading
 import time
 import numpy as np
 from get_financial_data import get_close_prices
+from execute_orders import buy, sell
 import alpaca_trade_api as tradeapi
 from API_keys import API_KEY, SECRET_KEY
 
@@ -20,14 +21,21 @@ def get_bollinger_bands(close_prices, window=20, num_std_dev=1.5):
     lower_band = mean - num_std_dev * std_dev
     return upper_band, lower_band
 
-def get_trade_status()
+# Get the status of an order after its been submitted
+def get_order_status(order_id):
+    specific_order = api.get_order_by_client_order_id(order_id)
+    print(specific_order)
 
+# Make the trading decisions
 def execute_trades():
     close_prices = get_close_prices('SPY')
-    bands = get_bollinger_bands(close_prices)
+    upper_band, lower_band = get_bollinger_bands(close_prices)
     
     # Make trade decision
     global current_position
-    if close_prices[-1] <
+    if current_position == 'neutral' and (close_prices[-1] <= lower_band):
+        order_id = buy('SPY', 1)
+        get_order_status(order_id)
+        
 
-trading_thread = threading.Thread(target=thread_function, args=(1,))
+trading_thread = threading.Thread(target=execute_trades)
