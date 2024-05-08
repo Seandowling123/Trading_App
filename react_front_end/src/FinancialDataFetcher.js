99367cd3a85db5a9a41a08ac899c48d142ff1843
Seandowling123 <dowling.sean103@gmail.com>
2024-05-08 18:31:49 +0100

    change to spy

--- react_front_end/src/FinancialDataFetcher.js
+++ react_front_end/src/FinancialDataFetcher.js
@@ -2,32 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import StockChart from './Plot_data.js';
 
-function HelloWorld() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios.get('http://51.20.79.10:8000/api/hello-world/')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>{message}</h1>
-      <p>{message}</p>
-    </div>
-  );
-}
-
 function Get_stock_chart() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://51.20.79.10:8000/api/historical_data/AAPL')
+    axios.get('http://127.0.0.1:8000/api/historical_data/SPY')
+    //axios.get('http://51.20.79.10:8000/api/historical_data/SPY')
       .then(response => {
         setMessage(response.data.financial_data);
         console.log('here');
