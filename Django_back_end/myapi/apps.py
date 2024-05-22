e2d4ceaeb952f37fbc1add156392603456faa744
Seandowling123 <dowling.sean103@gmail.com>
2024-05-22 15:15:56 +0100

    mostly working, adding trading aas a background task

--- Django_back_end/myapi/apps.py
+++ Django_back_end/myapi/apps.py
@@ -3,4 +3,9 @@ from django.apps import AppConfig
 class MyapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapi'
+    
+    """def ready(self):
+        # Import your function here and call it
+        from .finance_tools import mean_reversion
+        mean_reversion.run_trading_algorithm()"""
  
