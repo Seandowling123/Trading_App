537f94da88fb1b7f7ed8182dc316fe98d1c7317a
Seandowling123 <dowling.sean103@gmail.com>
2024-05-23 14:54:42 +0100

    refresh working

--- react_front_end/src/FinancialDataFetcher.js
+++ react_front_end/src/FinancialDataFetcher.js
@@ -3,57 +3,40 @@ import axios from 'axios';
 import StockChart from './Plot_data.js';
 import AccountDetails from './account_details.js';
 
+const fetchData = async (url, setData) => {
+  try {
+    const response = await axios.get(url);
+    setData(response.data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 
-
-function Get_stock_chart() {
+const Get_stock_chart = () => {
   const [historical_data, setHistorical_data] = useState('');
   const [trade_history, setTrade_history] = useState('');
   const [account_details, setAccount_details] = useState('');
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/historical_data/SPY')
-    //axios.get('http://51.20.79.10:8000/api/historical_data/SPY')
-      .then(response => {
-        setHistorical_data(response.data.financial_data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
+  const updateData = () => {
+    fetchData('http://127.0.0.1:8000/api/historical_data/SPY', (data) => setHistorical_data(data.financial_data));
+    fetchData('http://127.0.0.1:8000/api/trade_history', (data) => setTrade_history(data.trade_history));
+    fetchData('http://127.0.0.1:8000/api/account_details', (data) => setAccount_details(data.account_details));
+    //fetchData('http://51.20.79.10:8000/api/historical_data/SPY', (data) => setHistorical_data(data.financial_data));
+    //fetchData('http://51.20.79.10:8000/api/trade_history', (data) => setTrade_history(data.trade_history));
+    //fetchData('http://51.20.79.10:8000/api/account_details', (data) => setAccount_details(data.account_details));
+  };
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/trade_history')
-    //axios.get('http://51.20.79.10:8000/api/trade_history')
-      .then(response => {
-        setTrade_history(response.data.trade_history);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    updateData();
   }, []);
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/account_details')
-    //axios.get('http://51.20.79.10:8000/api/account_details')
-      .then(response => {
-        setAccount_details(response.data.account_details);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-  
-  
-  //console.log(historical_data);
-  //console.log(trade_history);
-  //console.log(account_details);
 
   return (
     <div id="trade-data-container">
-      <StockChart historical_data={historical_data} markersData={trade_history}/>
-      <AccountDetails account_details={account_details}/>
+      <button onClick={updateData}>Refresh Data</button>
+      <StockChart historical_data={historical_data} markersData={trade_history} />
+      <AccountDetails account_details={account_details} />
     </div>
   );
-}
+};
 
 export default Get_stock_chart;
