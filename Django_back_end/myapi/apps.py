230da27030cdd835b4abf62848c9c5bbbf7b7a05
Seandowling123 <dowling.sean103@gmail.com>
2024-05-23 14:04:12 +0100

    added trading algo in a tread

--- Django_back_end/myapi/apps.py
+++ Django_back_end/myapi/apps.py
@@ -1,11 +1,16 @@
 from django.apps import AppConfig
+import threading
+from .finance_tools import mean_reversion
 
 class MyapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapi'
     
-    """def ready(self):
-        # Import your function here and call it
-        from .finance_tools import mean_reversion
-        mean_reversion.run_trading_algorithm()"""
+    def ready(self):
+        
+        # Start the algo trading task in a separate thread
+        trading_algo_thread = threading.Thread(target=mean_reversion.run_trading_algorithm())
+        trading_algo_thread.daemon = True
+        trading_algo_thread.start()
+        
  
