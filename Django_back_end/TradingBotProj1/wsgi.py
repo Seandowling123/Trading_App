70ce4ee79934ce65bf332f0c0215e8150323f156
Seandowling123 <dowling.sean103@gmail.com>
2024-05-23 14:15:51 +0100

    algo running in thread

--- Django_back_end/TradingBotProj1/wsgi.py
+++ Django_back_end/TradingBotProj1/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import threading
+from myapi.finance_tools import mean_reversion
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TradingBotProj1.settings')
 
 application = get_wsgi_application()
+
+# Start the algo trading task in a separate thread
+trading_algo_thread = threading.Thread(target=mean_reversion.run_trading_algorithm)
+trading_algo_thread.daemon = True
+trading_algo_thread.start()
--- Django_back_end/myapi/apps.py
+++ Django_back_end/myapi/apps.py
@@ -1,16 +1,14 @@
 from django.apps import AppConfig
-import threading
-from .finance_tools import mean_reversion
 
 class MyapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapi'
     
-    def ready(self):
+    """def ready(self):
         
         # Start the algo trading task in a separate thread
         trading_algo_thread = threading.Thread(target=mean_reversion.run_trading_algorithm())
         trading_algo_thread.daemon = True
-        trading_algo_thread.start()
+        trading_algo_thread.start()"""
         
  
