87de9a785ad10eaa720ee5c8a428e1cbc0ec804a
Seandowling123 <dowling.sean103@gmail.com>
2024-05-23 15:54:41 +0100

    small changes to trading

--- Django_back_end/myapi/finance_tools/mean_reversion.py
+++ Django_back_end/myapi/finance_tools/mean_reversion.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .get_financial_data import get_close_prices
 from .execute_orders import buy, sell, get_order_data, save_trade_to_csv, save_trade_to_json
@@ -9,7 +10,7 @@ current_position = 'Sold'
 bought_price = None
 
 # Calculate the bollinger bands for the current data
-def get_bollinger_bands(close_prices, window=20, num_std_dev=.5):
+def get_bollinger_bands(close_prices, window=20, num_std_dev=1.2):
     mean = np.mean(close_prices[-window:])
     std_dev = np.std(close_prices[-window:])
     
@@ -20,37 +21,48 @@ def get_bollinger_bands(close_prices, window=20, num_std_dev=.5):
 
 # Make the trading decisions
 def execute_trades():
-    print('Evaluating position.')
     close_prices = list(get_close_prices('SPY'))
     upper_band, lower_band = get_bollinger_bands(close_prices)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     # Make trade decision
     global current_position
     global bought_price
     
-    # Execute buy
-    print(current_position)
-    if current_position == 'Sold' and (close_prices[-1] <= lower_band):
-        order_id = buy('SPY', 1)
-        order_data = get_order_data(order_id)
-        if order_data.status  == 'filled':
-            current_position = 'Bought'
-            bought_price = float(order_data.filled_avg_price)
-            save_trade_to_csv(order_data)
-        
-        save_trade_to_json(order_data)
-        print(order_data.status)
-        
-    # Execute sell 
-    elif current_position == 'Bought' and (close_prices[-1] >= bought_price) and (close_prices[-1] >= upper_band):
-        order_id = sell('SPY', 1)
-        order_data = get_order_data(order_id)
-        if order_data.status  == 'filled':
-            current_position = 'Sold'
-            bought_price = None
-            save_trade_to_csv(order_data)
-        print(order_data.status)
-    else: print('No trade available:', close_prices[-1], lower_band, upper_band)
+    # check if there is enough data
+    if len(close_prices) >= 20:
+    
+        # Execute buy
+        if current_position == 'Sold' and (close_prices[-1] <= lower_band):
+            order_id = buy('SPY', 1)
+            print(f"[{current_time}] Trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
+                  f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
+            order_data = get_order_data(order_id)
+            if order_data.status  == 'filled':
+                current_position = 'Bought'
+                bought_price = float(order_data.filled_avg_price)
+                save_trade_to_csv(order_data)
+            print(f"Order status: {order_data.status}")
+            
+        # Execute sell 
+        elif current_position == 'Bought' and (close_prices[-1] >= bought_price) and (close_prices[-1] >= upper_band):
+            order_id = sell('SPY', 1)
+            print(f"[{current_time}] Trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
+                  f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
+            order_data = get_order_data(order_id)
+            if order_data.status  == 'filled':
+                current_position = 'Sold'
+                bought_price = None
+                save_trade_to_csv(order_data)
+            print(f"Order status: {order_data.status}")
+        else:
+            # Format numbers to two decimal places
+            last_close_formatted = "{:.2f}".format(close_prices[-1])
+            lower_band_formatted = "{:.2f}".format(lower_band)
+            upper_band_formatted = "{:.2f}".format(upper_band)
+            print(f"[{current_time}] No trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
+                  f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
+
 
 # Execute trades every minute
 def run_trading_algorithm():
