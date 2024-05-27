ed60e35ff3a0c1a96250b8459990d207d90dee66
Seandowling123 <dowling.sean103@gmail.com>
2024-05-27 14:29:12 +0100

    change static url

--- Django_back_end/TradingBotProj1/settings.py
+++ Django_back_end/TradingBotProj1/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/'
 
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "build/static"),
+    os.path.join(BASE_DIR, "build"),
 )
 
 # Default primary key field type
