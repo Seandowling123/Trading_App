09e82365e78e06defc1f16938c561e964241c045
Seandowling123 <dowling.sean103@gmail.com>
2024-05-27 22:25:37 +0100

    move favicon url

--- Django_back_end/TradingBotProj1/urls.py
+++ Django_back_end/TradingBotProj1/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon_custom.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapi.urls'))
+    path('api/', include('myapi.urls')),
+    re_path(r'^favicon\.ico$', favicon_view)
 ]
--- Django_back_end/myapi/urls.py
+++ Django_back_end/myapi/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
 from . import views
-from django.urls import re_path
-from django.views.generic.base import RedirectView
-
-favicon_view = RedirectView.as_view(url='/static/favicon_custom.ico', permanent=True)
 
 # Django_back_end directory
 # python manage.py runserver
 # sudo python3 manage.py runserver 0.0.0.0:8000
 
 urlpatterns = [
-    re_path(r'^favicon\.ico$', favicon_view),
     path('hello-world/', views.hello_world, name='hello_world'),
     path('asdf/', views.asdf),
     path('historical_data/<str:ticker>', views.historical_data),
