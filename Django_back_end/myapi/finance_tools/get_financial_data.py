41259d889b71bd5412fe1e332a7f63acb006c9cc
Seandowling123 <dowling.sean103@gmail.com>
2024-05-31 13:28:40 +0100

    try catches

--- Django_back_end/myapi/finance_tools/get_financial_data.py
+++ Django_back_end/myapi/finance_tools/get_financial_data.py
@@ -37,19 +37,27 @@ def get_historical_data(ticker, timescale='day'):
 
 # Get stock closing prices
 def get_close_prices(ticker, timescale='day'):
-    data = get_historical_data(ticker, timescale)
-    close_prices = data['Close']
-    return close_prices
+    try:
+        data = get_historical_data(ticker, timescale)
+        close_prices = data['Close']
+        return close_prices
+    except Exception as e: 
+        logging.info(f'Error getting data for {ticker}: {e}')
+        return None
 
 # Calculate Bollinger Bands
 def calculate_bollinger_bands(close, window_size=20, num_std_dev=2):
-    rolling_mean = close.rolling(window=window_size).mean()
-    rolling_std = close.rolling(window=window_size).std()
+    try:
+        rolling_mean = close.rolling(window=window_size).mean()
+        rolling_std = close.rolling(window=window_size).std()
 
-    # Calculate upper and lower Bollinger Bands
-    upper_band = rolling_mean + num_std_dev * rolling_std
-    lower_band = rolling_mean - num_std_dev * rolling_std
-    return upper_band, lower_band
+        # Calculate upper and lower Bollinger Bands
+        upper_band = rolling_mean + num_std_dev * rolling_std
+        lower_band = rolling_mean - num_std_dev * rolling_std
+        return upper_band, lower_band
+    except Exception as e: 
+        logging.info(f'Error calculating Bollinger Bands: {e}')
+        return None
 
 # Return close price time series with Bollinger Bands
 def get_close_with_bands(ticker, timescale='day'):
