ab6d58c97c7151212aacaa3860f40b7d80fea0f9
Seandowling123 <dowling.sean103@gmail.com>
2024-05-31 13:30:04 +0100

    more try catch

--- Django_back_end/myapi/finance_tools/mean_reversion.py
+++ Django_back_end/myapi/finance_tools/mean_reversion.py
@@ -19,43 +19,48 @@ def get_bollinger_bands(close_prices, window=20, num_std_dev=2):
 
 # Make the trading decisions
 def execute_trades():
-    close_prices = list(get_close_prices('SPY'))
-    upper_band, lower_band = get_bollinger_bands(close_prices)
-    current_time = datetime.now().strftime("%d/%b/%Y %H:%M:%S")
-    
-    # Format numbers to two decimal places
-    last_close_formatted = "{:.2f}".format(close_prices[-1])
-    lower_band_formatted = "{:.2f}".format(lower_band)
-    upper_band_formatted = "{:.2f}".format(upper_band)
-    
-    # Get current trade position
-    current_position, bought_price = get_current_position()
-    
-    # check if there is enough data
-    if len(close_prices) >= 20:
-    
-        # Execute buy
-        if current_position == 'Sold' and (close_prices[-1] <= lower_band):
-            order_id = buy('SPY', 1)
-            logging.info(f"[{current_time}] Trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
-                  f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
-            order_data = get_order_data(order_id)
-            if order_data.status  == 'filled':
-                save_trade_to_csv(order_data)
-            logging.info(f"Order status: {order_data.status}")
-            
-        # Execute sell 
-        elif current_position == 'Bought' and (close_prices[-1] >= bought_price) and (close_prices[-1] >= upper_band):
-            order_id = sell('SPY', 1)
-            logging.info(f"[{current_time}] Trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
-                  f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
-            order_data = get_order_data(order_id)
-            if order_data.status  == 'filled':
-                save_trade_to_csv(order_data)
-            logging.info(f"Order status: {order_data.status}")
-        else:
-            logging.info(f"[{current_time}] No trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
-                  f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
+    try:
+        close_prices = list(get_close_prices('SPY'))
+        upper_band, lower_band = get_bollinger_bands(close_prices)
+        current_time = datetime.now().strftime("%d/%b/%Y %H:%M:%S")
+        
+        # Format numbers to two decimal places
+        last_close_formatted = "{:.2f}".format(close_prices[-1])
+        lower_band_formatted = "{:.2f}".format(lower_band)
+        upper_band_formatted = "{:.2f}".format(upper_band)
+        
+        # Get current trade position
+        current_position, bought_price = get_current_position()
+        
+        # check if there is enough data
+        if len(close_prices) >= 20:
+        
+            # Execute buy
+            if current_position == 'Sold' and (close_prices[-1] <= lower_band):
+                order_id = buy('SPY', 1)
+                logging.info(f"[{current_time}] Trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
+                    f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
+                order_data = get_order_data(order_id)
+                if order_data.status  == 'filled':
+                    save_trade_to_csv(order_data)
+                logging.info(f"Order status: {order_data.status}")
+                
+            # Execute sell 
+            elif current_position == 'Bought' and (close_prices[-1] >= bought_price) and (close_prices[-1] >= upper_band):
+                order_id = sell('SPY', 1)
+                logging.info(f"[{current_time}] Trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
+                    f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
+                order_data = get_order_data(order_id)
+                if order_data.status  == 'filled':
+                    save_trade_to_csv(order_data)
+                logging.info(f"Order status: {order_data.status}")
+            else:
+                logging.info(f"[{current_time}] No trade available. Last close: {last_close_formatted}, Lower Band: {lower_band_formatted}, "
+                    f"Upper Band: {upper_band_formatted}, Current Position: {current_position}")
+                
+    except Exception as e: 
+        logging.info(f'Error executing trade: {e}')
+        return None
 
 
 # Execute trades every minute
