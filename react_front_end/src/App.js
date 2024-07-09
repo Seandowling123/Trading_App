ff6ee3e87e2d300b4b6d03f24ce4d29a094f0117
Seandowling123 <dowling.sean103@gmail.com>
2024-07-09 14:49:47 +0100

    add notice to dashboard

--- react_front_end/src/App.js
+++ react_front_end/src/App.js
@@ -13,6 +13,7 @@ function App() {
       <div id="title">
         <h1>Daily Trading Activity</h1>
       </div>
+      <div id="notice"><p>Real time algorithmic trading information for the ticker SPY. Note that the algorithm currently operates on a paper trading account with a high balance for compliance with trading regulations.</p></div>
       < Get_stock_chart />
     </div>
   );
