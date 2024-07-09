2affa9334f588f1dc883992dd2de9af016a52590
Seandowling123 <dowling.sean103@gmail.com>
2024-07-09 14:23:31 +0100

    debugging

--- Django_back_end/myapi/finance_tools/get_trade_history.py
+++ Django_back_end/myapi/finance_tools/get_trade_history.py
@@ -42,8 +42,8 @@ def get_trade_history():
         else:
             clock = api.get_clock()
             last_close = api.get_calendar(start=clock.timestamp.date() - pd.Timedelta(days=1), end=clock.timestamp.date())[0].date
-            logging.info(last_close)
-            filtered_df = df[df['datetime'].dt.date == last_close]
+            logging.info(last_close.date())
+            filtered_df = df[df['datetime'].dt.date == last_close.date()]
             logging.info(filtered_df)
         
         # Close the connection
