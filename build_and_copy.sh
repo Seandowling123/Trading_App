799c88b740ac5e530cbd853d4e1a5986d9601cf5
Seandowling123 <dowling.sean103@gmail.com>
2024-07-16 15:35:36 +0100

    bug fix

--- build_and_copy.sh
+++ build_and_copy.sh
@@ -4,5 +4,5 @@
 rm -r Django_back_end/build/*
 
 # Copy contents of react_front_end/build/ to Django_back_end/build/
-cp -r react_front_end/build/* 
+cp -r react_front_end/build/* Django_back_end/build/
 cp Django_back_end/build/favicon_custom.ico Django_back_end/build/static/
